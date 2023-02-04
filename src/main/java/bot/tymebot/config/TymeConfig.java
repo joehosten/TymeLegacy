d7b806a13aac3348f9860be20b46b1759eca1e7b
Negative <negggative@gmail.com>
2023-02-04 15:52:42 -0400

    Add "parent-server" configuration value

--- src/main/java/bot/tymebot/config/TymeConfig.java
+++ src/main/java/bot/tymebot/config/TymeConfig.java
@@ -10,6 +10,11 @@ public class TymeConfig {
     @SerializedName("token")
     private String botToken;
 
+    // Represents the Discord Server ID of the "Parent Server", you can
+    // consider this an "admin server"
+    @SerializedName("parent-server-id")
+    private String parentServer;
+
     @SerializedName("devIds")
     private String[] devIds;
 
